1ff4e2f3eb9699aa0b438a627439b9efc138467b
tuhs1985 <77306309+tuhs1985@users.noreply.github.com>
2025-06-25 00:10:00 -0400

    Initial Commits

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// https://vite.dev/config/
 export default defineConfig({
-  base: '/', // ← this is the fix
   plugins: [react()],
-});
+})
