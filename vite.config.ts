54c104190ccc4998111591d6907e243cad01a46b
tuhs1985 <77306309+tuhs1985@users.noreply.github.com>
2025-06-27 16:00:55 -0400

    Config Changes

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// CHANGE 'pf2e-crafting' to your repo name
 export default defineConfig({
   plugins: [react()],
-  base: '/pf2e-crafting/',  // <--- add this line
+  base: '/pf2e-crafting/', // <-- This must match your repo name
 });
